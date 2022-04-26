79c1ca8582a4296f992aef752317c141f2766633
Fazry <lhuqita.fazry@gmail.com>
2022-04-26 09:04:22 +0700

    change loss function

--- losses/mse.py
+++ losses/mse.py
@@ -1,7 +1,9 @@
 from torch import nn
+import torchmetrics
 
 
 def mse_loss(input, target):
-    mse = nn.MSELoss()
-    mae = nn.L1Loss()
-    return mse(input, target) + mae(input, target)
+    #mse = nn.MSELoss()
+    mae = torchmetrics.MeanAbsoluteError()
+    #return mse(input, target) + mae(input, target)
+    return mae(input, target)
