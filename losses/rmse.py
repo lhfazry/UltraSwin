25a03d4f8e225d4dacc0b0e9118e73d5f41c5078
Fazry <lhuqita.fazry@gmail.com>
2022-05-15 20:33:10 +0700

    fixing bugs

---
+++ losses/rmse.py
@@ -0,0 +1,15 @@
+from torch import nn
+import torch
+import torchmetrics
+
+class RMSE(torchmetrics.Metric):
+    def __init__(self):
+        self.add_state("sum_squared_errors", torch.tensor(0), dist_reduce_fx='sum')
+        self.add_state("n_observations", torch.tensor(0), dist_reduce_fx='sum')
+
+    def update(self, preds, target):
+        self.sum_squared_errors += torch.sum((preds - target) ** 2)
+        self.n_observations += preds.numel()
+
+    def compute(self):
+        return torch.sqrt(self.sum_squared_errors / self.n_observations)
