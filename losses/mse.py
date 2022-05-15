5a596317ad1cb54505e8c0ce20886545cdf9ea25
Fazry <lhuqita.fazry@gmail.com>
2022-05-15 20:41:10 +0700

    fixing bugs

--- losses/mse.py
+++ losses/mse.py
@@ -7,4 +7,4 @@ def mse_loss(input, target):
     #mae = nn.L1Loss() #torchmetrics.MeanAbsoluteError()
     #return mse(input, target) + mae(input, target)
     hubber_loss = nn.HuberLoss()
-    return hubber_loss(input, target)
+    return mse(input, target)
