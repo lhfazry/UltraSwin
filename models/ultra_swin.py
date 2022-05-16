8d5f68a9efe3d725ed243e11af2aa4d78ad0513e
Fazry <lhuqita.fazry@gmail.com>
2022-05-16 14:25:17 +0700

    fixing mse

--- models/ultra_swin.py
+++ models/ultra_swin.py
@@ -219,6 +219,6 @@ class UltraSwin(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=10e-4)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.005, verbose=True)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1, verbose=True)
 
         return [optimizer], [lr_scheduler]
