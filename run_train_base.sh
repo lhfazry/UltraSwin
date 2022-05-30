1ef067de4e330038a4e29d1330152d1eeae7fd3a
Fazry <lhuqita.fazry@gmail.com>
2022-05-30 09:43:48 +0700

    change to 3D pretrained

--- run_train_base.sh
+++ run_train_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup python main.py \
+CUDA_VISIBLE_DEVICES=3 python main.py \
     --data_dir=/workspace/EchoNet \
     --pretrained=pretrained/swin_base_patch244_window877_kinetics400_22k.pth \
     --batch_size=8 \
