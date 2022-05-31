3597b52216f05085642891cbf9659043c353c9a2
Fazry <lhuqita.fazry@gmail.com>
2022-05-31 16:07:49 +0700

    fix bug

--- run_train_base.sh
+++ run_train_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=3 python main.py \
-    --data_dir=/workspace/UltraSwin/EchoNet \
+    --data_dir=/workspace/EchoNet \
     --pretrained=pretrained/swin_base_patch244_window877_kinetics400_22k.pth \
     --batch_size=8 \
     --num_workers=4 \
