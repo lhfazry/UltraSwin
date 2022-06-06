1114ac559731bf2835966afb870f40671c41ea51
Fazry <lhuqita.fazry@gmail.com>
2022-06-06 18:38:36 +0700

    fixing bugs

--- run_validate_base.sh
+++ run_validate_base.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode=validate \
     --data_dir=/workspace/EchoNet \
-    --ckpt_path=lightning_logs/ultraswin/version_18/checkpoints/epoch=19-step=8960.ckpt \
+    --ckpt_path=lightning_logs/ultraswin/version_26/checkpoints/epoch\=4-step\=8955.ckpt \
     --batch_size=2 \
     --num_workers=4 \
     --accelerator=gpu 
