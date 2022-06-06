268d68a7da3c871ae22ee9e36c0f6561fbead15b
Fazry <lhuqita.fazry@gmail.com>
2022-06-06 18:48:59 +0700

    add test script

--- run_test_base.sh
+++ run_test_base.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
+    --mode=test \
     --data_dir=/workspace/EchoNet \
-    --pretrained=pretrained/swin_base_patch244_window877_kinetics400_22k.pth \
     --ckpt_path=lightning_logs/ultraswin/version_26/checkpoints/epoch\=4-step\=8955.ckpt \
-    --batch_size=4 \
+    --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=base \
-    --max_epoch=100
+    --log
