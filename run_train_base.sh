7eb5a91db95d77ca22cdb3c0fce1d2b5951e92b2
Fazry <lhuqita.fazry@gmail.com>
2022-06-07 04:20:26 +0700

    fixing ef regressor

--- run_train_base.sh
+++ run_train_base.sh
@@ -2,9 +2,8 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --data_dir=/workspace/EchoNet \
     --pretrained=pretrained/swin_base_patch244_window877_kinetics400_22k.pth \
-    --ckpt_path=lightning_logs/ultraswin/version_26/checkpoints/epoch\=4-step\=8955.ckpt \
     --batch_size=4 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=base \
-    --max_epoch=100
+    --max_epoch=1
