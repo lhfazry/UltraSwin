cd97316faeaa68108997a283a8a688a13b95f84c
Fazry <lhuqita.fazry@gmail.com>
2022-06-08 06:39:17 +0700

    fixing bug

--- run_test_base.sh
+++ run_test_base.sh
@@ -2,9 +2,8 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode=test \
     --data_dir=/workspace/EchoNet \
-    --ckpt_path=lightning_logs/ultraswin/version_36/checkpoints/epoch\=10-step\=9856.ckpt \
+    --ckpt_path=lightning_logs/ultraswin/version_57/checkpoints/epoch\=0-step\=896.ckpt \
     --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
-    --variant=base \
-    --log
+    --variant=base
