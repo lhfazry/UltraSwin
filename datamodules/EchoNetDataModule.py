f76bcc30ac8285bf1c9e9f0d488ad254ce4ea6fb
Fazry <lhuqita.fazry@gmail.com>
2022-06-08 19:50:07 +0700

    fixing bug

--- datamodules/EchoNetDataModule.py
+++ datamodules/EchoNetDataModule.py
@@ -31,7 +31,7 @@ class EchoNetDataModule(pl.LightningDataModule):
             self.train_set = EchoSet(root=self.data_dir,
                                 split="train",
                                 pad=8,
-                                augmented=True,
+                                augmented=False,
                                 #max_data=1000,
                                 random_clip=False,
                                 dataset_mode=self.dataset_mode)
