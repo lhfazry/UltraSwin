e921cf74e1e3623d9969e56fa14a2ee8a96f876a
Fazry <lhuqita.fazry@gmail.com>
2022-06-08 14:05:53 +0700

    add prog bar

--- models/ultra_swin.py
+++ models/ultra_swin.py
@@ -228,7 +228,7 @@ class UltraSwin(pl.LightningModule):
         #self.train_mae(y_hat, ejection)
         #r2loss = r2_loss(y_hat, ejection)
 
-        self.log('loss', loss, on_step=True, on_epoch=True, batch_size=self.batch_size)
+        self.log('loss', loss, on_epoch=True, batch_size=self.batch_size)
         #self.log('train_mse', self.train_mse, on_step=True, on_epoch=False, batch_size=self.batch_size)
         #self.log('train_mae', self.train_mse, on_step=True, on_epoch=False, batch_size=self.batch_size)
         #self.log('train_r2', r2loss, on_step=True, on_epoch=False, batch_size=self.batch_size)
@@ -254,10 +254,10 @@ class UltraSwin(pl.LightningModule):
         self.val_r2(ef_pred, ef_label)
         #r2loss = r2_score(y_hat, ejection)
 
-        self.log('val_loss', loss, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('val_rmse', self.val_rmse, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('val_mae', self.val_mae, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('val_r2', self.val_r2, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('val_loss', loss, on_step=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('val_rmse', self.val_rmse, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('val_mae', self.val_mae, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('val_r2', self.val_r2, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
 
         #tensorboard_logs = {'loss':{'val': loss.detach() } }
         #return {"val_loss": loss, 'log': tensorboard_logs }
@@ -282,10 +282,10 @@ class UltraSwin(pl.LightningModule):
         #r2loss = r2_score(y_hat, ejection)
 
         self.log('test_loss', loss, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('test_rmse', self.test_rmse, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('test_mse', self.test_mse, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('test_mae', self.test_mae, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
-        self.log('test_r2', self.test_r2, on_step=True, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('test_rmse', self.test_rmse, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('test_mse', self.test_mse, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('test_mae', self.test_mae, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
+        self.log('test_r2', self.test_r2, on_epoch=True, batch_size=self.batch_size, prog_bar=True)
 
         return {"test_loss": loss}
 
