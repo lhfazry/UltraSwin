f1636d28ec050d385c19230ece735fd4891b34e0
Fazry <lhuqita.fazry@gmail.com>
2022-06-09 06:23:33 +0700

    fixing bug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", type=str, default="train", help="Train or test")
-parser.add_argument("--pretrained", type=str, default="pretrained/swin_base_patch4_window7_224_22k.pth", help="File pretrained swin")
+parser.add_argument("--pretrained", type=str, default=None, help="File pretrained swin")
 parser.add_argument("--data_dir", type=str, default="datasets/EchoNet", help="Path ke datasets")
 parser.add_argument("--batch_size", type=int, default=8, help="Batch size")
 #parser.add_argument("--embed_dim", type=int, default=128, help="Embed dimension")
