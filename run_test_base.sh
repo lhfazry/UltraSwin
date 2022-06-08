d003c301872e7cc02452c80c5896af0e8ee54a20
Fazry <lhuqita.fazry@gmail.com>
2022-06-09 05:04:23 +0700

    add small model

--- run_test_base.sh
+++ run_test_base.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode=test \
     --data_dir=/workspace/EchoNet \
-    --ckpt_path=lightning_logs/ultraswin/version_79/checkpoints/epoch\=0-step\=896.ckpt \
+    --ckpt_path=lightning_logs/ultraswin/version_81/checkpoints/epoch\=19-step\=17920.ckpt \
     --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
