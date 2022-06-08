66e9a6aa5582995904e2661de9dbda9482274315
Fazry <lhuqita.fazry@gmail.com>
2022-06-09 04:23:40 +0700

    add small model

---
+++ run_train_small.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --data_dir=/workspace/EchoNet \
+    --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
+    --batch_size=8 \
+    --num_workers=4 \
+    --accelerator=gpu \
+    --variant=small \
+    --max_epoch=20
