44ada9a6e143d285ab9aec25de41b4009bb7e043
Fazry <lhuqita.fazry@gmail.com>
2022-06-09 11:11:50 +0700

    test frozen

--- run_train_base_fz3.sh
+++ run_train_base_fz3.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --data_dir=/workspace/EchoNet \
     --pretrained=pretrained/swin_base_patch4_window7_224_22k.pth \
-    --batch_size=16 \
+    --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=base \
