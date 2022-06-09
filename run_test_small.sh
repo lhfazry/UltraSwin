a065de9e9bdc8991e4b738bdcfdef370cf134ff4
Fazry <lhuqita.fazry@gmail.com>
2022-06-10 04:05:17 +0700

    test small

--- run_test_small.sh
+++ run_test_small.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode=test \
     --data_dir=/workspace/EchoNet \
-    --ckpt_path=lightning_logs/ultraswin/version_83/checkpoints/epoch=19-step=8960.ckpt \
+    --ckpt_path=lightning_logs/ultraswin/version_88/checkpoints/epoch\=17-step\=16128.ckpt \
     --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
